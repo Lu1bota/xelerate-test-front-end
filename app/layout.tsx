511db6f7391043490d3f6d3833a608ccd931a9e1
Danylo Liubota <daniil.lyubota@gmail.com>
2025-08-09 15:38:08 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Raleway } from "next/font/google";
+import "./globals.scss";
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-raleway",
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Досягнення | XELERATE",
+  description:
+    "Відстежуйте свої досягнення, відкривайте нові рівні та мотивуйте себе розвиватися щодня.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${raleway.variable}`}>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
